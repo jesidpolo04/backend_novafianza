1c39423013b2e13aff506d33becba6008cff226e
Jesid Polo <jesing482@gmail.com>
2023-08-02 19:56:30 -0500

    proveiendo cliente http

--- app/Presentacion/Reportes/ControladorReportes.ts
+++ app/Presentacion/Reportes/ControladorReportes.ts
@@ -2,12 +2,13 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schemaFiltroColocacion } from './Validadores/ValidadorFiltroColocacion'
 import { ServicioReportes } from 'App/Dominio/Datos/Servicios/ServicioReportes'
 import { RepositorioReportesSafix } from 'App/Infraestructura/Implementacion/Servicios/RepositorioReportesSafix'
+import { ClienteHttpAxios } from 'App/Infraestructura/ClientesHttp/ClienteHttpAxios'
 
 export default class ControladorReportes {
   private servicio: ServicioReportes
   
   constructor () {
-    this.servicio = new ServicioReportes( new RepositorioReportesSafix() )
+    this.servicio = new ServicioReportes( new RepositorioReportesSafix( new ClienteHttpAxios() ) )
   }
 
   async colocacion ({ request, response }: HttpContextContract) {

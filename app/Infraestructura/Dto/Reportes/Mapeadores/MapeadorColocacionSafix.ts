c77b24cd4fa2c3ba4d09043ff52fa0db41f75286
Jesid Polo <jesing482@gmail.com>
2023-10-11 15:58:52 -0500

    ordenando coberturas disponibles

--- app/Infraestructura/Dto/Reportes/Mapeadores/MapeadorColocacionSafix.ts
+++ app/Infraestructura/Dto/Reportes/Mapeadores/MapeadorColocacionSafix.ts
@@ -63,6 +63,7 @@ export class MapeadorColocacionSafix {
         })
 
         colocacion.ordenarFianzasNetas()
+        colocacion.ordernarCoberturasDisponibles()
 
         return colocacion
     }
